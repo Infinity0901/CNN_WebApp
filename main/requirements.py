b70656586fe1cf173850f6f4d5a73acb5f0ed61c
Infinity0901 <147404427+Infinity0901@users.noreply.github.com>
2024-09-19 15:43:50 +0200

    requirements

---
+++ main/requirements.py
@@ -0,0 +1,32 @@
+# REQUIREMENTS
+
+# 1. Mettre deux espaces entre deux focntions ou classes à part dans la cas suivant
+
+# class A: 
+#     def __init__(self) -> None:
+#         pass
+    
+#     def foo(self) -> None: 
+#         pass
+    
+#     def bar(self) -> None:
+#         pass
+
+
+# Mettre les bibliothèques dans l'ordre aphablétique
+# from PIL import Image
+# from requests import Response
+
+
+# import numpy as np
+# import os 
+
+# Contraintes à suivre pour les rendus
+# from __future__ import annotations
+
+# def foo(x : float) -> list[float]:
+#     """Foo
+#     The foo function is creating a list 
+#     """
+    
+#     return [x, 2 * x, 3*x]
